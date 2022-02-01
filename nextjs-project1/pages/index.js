ed520c36531524128a89e8d2b9c1d43f4fcf967d
Prakhar Dhama <pdhama161@gmail.com>
2022-02-01 09:57:02 +0530

    till layout and basic css of project1

--- nextjs-project1/pages/index.js
+++ nextjs-project1/pages/index.js
@@ -1,16 +1,14 @@
-import Link from 'next/link'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, sunt nostrum? Explicabo aliquam beatae voluptas est autem corporis, sequi amet.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore vero ipsum ducimus soluta ea fugit tempora non quia asperiores.</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, sunt nostrum? Explicabo aliquam beatae voluptas est autem corporis, sequi amet.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore vero ipsum ducimus soluta ea fugit tempora non quia asperiores.</p>
       <Link href="/people">
-        See People's Listing
+        <a className={styles.btn}>See People's Listing</a>
       </Link>
     </div>
   )
